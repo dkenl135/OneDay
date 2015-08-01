675c978cda6d6715924f87cfab98e4b08d9b99b0
IronFactory <wind.dong.dream@gmail.com>
2015-08-02 01:57:18 +0900

    Ver0.0.1
    로그인

---
+++ app/src/main/java/com/windsoft/oneday/fragment/LoginFragment.java
@@ -0,0 +1,24 @@
+package com.windsoft.oneday.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.windsoft.oneday.R;
+
+/**
+ * Created by ironFactory on 2015-08-02.
+ */
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return rootView;
+    }
+}
